5ae6c0464c97de9686f342a202b1b1e703437535
DesertStormII <119744433+DesertStormII@users.noreply.github.com>
2023-02-14 19:28:18 +0500

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int array1[] = { 12,32,23,21,45,67 };
+	for (int i = 0; i < 6; i++) {
+		cout << array1[i] << endl;
+	}
+
+
+	return 0;
+}
